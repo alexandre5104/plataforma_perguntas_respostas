da3ee915fc5cb8417dca379ce7cb86086c5c15b3
Alexandre Batista <alexandre.dev23@gmail.com>
2023-07-31 21:27:47 -0300

    Criando minha primeira rota

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("alexandre!")
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) =>{
+    response.send("Minha primeira rota de teste")
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor rodando!")
+})
